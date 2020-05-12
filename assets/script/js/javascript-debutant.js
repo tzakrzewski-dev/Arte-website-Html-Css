79ccb612e504aa3fab02871ac6a8cde1dea48cca
Teddy Zakrzewski <teddy.zakrzewski@outlook.fr>
2020-05-12 17:51:19 +0200

    first commit on github

---
+++ assets/script/js/javascript-debutant.js
@@ -0,0 +1,7 @@
+var nom = 'Zakrzewski';
+var prenom = 'Teddy';
+var age = 43;
+var adresseEmail = 'teddy.zakrzewski@outlook.fr';
+
+console.log(nom + ' ' + prenom + ' ' + age + ' ' + adresseEmail);
+alert(nom + ' ' + prenom);
